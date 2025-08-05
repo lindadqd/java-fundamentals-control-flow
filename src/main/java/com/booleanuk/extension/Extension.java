640d3f60430c8def33d176e67f2a22f3427f2212
Linda Do <lindado_98@hotmail.com>
2025-08-05 14:11:20 +0200

    completed

--- src/main/java/com/booleanuk/extension/Extension.java
+++ src/main/java/com/booleanuk/extension/Extension.java
@@ -2,6 +2,8 @@ package com.booleanuk.extension;
 
 import com.booleanuk.helpers.ExtensionBase;
 
+import java.lang.reflect.Array;
+
 public class Extension extends ExtensionBase {
     /*  1.
         We're going to improve our cake baking capabilities!
@@ -13,7 +15,11 @@ public class Extension extends ExtensionBase {
         "The cake is still baking!" if there are any remaining minutes left,
         and "The timer finished ages ago!" if the remaining minutes is a negative number
      */
-
+        public String timerStatus(int minutesLeft) {
+            if (minutesLeft == 0) return "The cake is ready!";
+            if (minutesLeft < 0) return "The timer finished ages ago!";
+            return "The cake is still baking!";
+        }
 
 
 
@@ -27,7 +33,13 @@ public class Extension extends ExtensionBase {
         If a prep time of 0 is provided, the method should assume each ingredient takes 2 minutes to prepare.
      */
 
+    public int estimatePrepTime (String[] ingredients, int minutes){
+        int prepTime = minutes;
+        if (minutes == 0) {
+            prepTime = 2; }
 
+        return prepTime * ingredients.length;
+    }
 
     /*  3.
         Create a method named calculateGramsOfSugar that accepts two parameters:
@@ -40,7 +52,20 @@ public class Extension extends ExtensionBase {
 
         You may need to use programming techniques we have yet to cover in the course to solve this task.
      */
+    public int calculateGramsOfSugar (String[] ingredients, int numberOfLayers) {
+        boolean hasSugar = false;
+        String sugar = "sugar";
 
+        for (String s : ingredients) {
+            if (s.equals("sugar")) {
+                hasSugar = true;
+                break;
+            }
+        }
+
+        if (hasSugar) return 100 * numberOfLayers;
+        return 0;
+    }
 
 
 
